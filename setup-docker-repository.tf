5e783563ccbddacab357cbc72c6f3874f2b2b985
Victor Argento <vargento@sklum.com>
2022-02-25 12:36:57 +0100

    feat: docker hub terraform

---
+++ setup-docker-repository.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    dockerhub = {
+      source = "BarnabyShearer/dockerhub"
+      version = "0.0.8"
+    }
+  }
+}
+
+provider "dockerhub" {
+  username = var.docker_user
+  password = var.docker_pass
+}
+
+resource "dockerhub_repository" "repository" {
+  name = var.repo_name
+  namespace = "indecemisl"
+  private = true
+}
+
+variable "docker_user" {
+  type = string
+}
+
+variable "docker_pass" {
+  type = string
+}
+
+variable "repo_name" {
+  type = string
+}
